0fa21d69eea83d44746d52918fc354f0d09a28f8
King1219 <roymakhlouf14@hotmail.be>
2021-09-12 20:28:40 +0200

    Background and Wall images

--- main.c
+++ main.c
@@ -2,9 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+SDL_Texture* SDL_CreateTextureFromImage(char *path, SDL_Renderer *renderer){
+    SDL_Surface *tmp = SDL_LoadBMP(path);
+    if (tmp == NULL){
+        fprintf(stderr, "Error in SDL_LoadBMP : %s\n", SDL_GetError());
+        SDL_Quit();
+    }
+    SDL_Texture *tmp2 = SDL_CreateTextureFromSurface(renderer, tmp);
+    SDL_FreeSurface(tmp);
+    if (tmp2 == NULL){
+        fprintf(stderr, "Error in SDL_CreateTextureFromSurface : %s\n", SDL_GetError());
+        SDL_DestroyTexture(tmp2);
+        SDL_Quit();
+    }
+    SDL_Rect dst = {0, 0, 0, 0};
+    SDL_QueryTexture(tmp2, NULL, NULL, &dst.w, &dst.h);
+    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, dst.w, dst.h);
+    if (texture == NULL){
+        fprintf(stderr, "Error in SDL_CreateTexture : %s\n", SDL_GetError());
+        SDL_DestroyTexture(texture);
+        SDL_Quit();
+    }
+    SDL_SetRenderTarget(renderer, texture);
+    SDL_RenderCopy(renderer, tmp2, NULL, NULL);
+    SDL_DestroyTexture(tmp2);
+    SDL_SetRenderTarget(renderer, NULL);
+    return texture;
+}
+
+void SDL_RenderDrawWall(SDL_Renderer *renderer, int x, int y){
+    SDL_Texture *texture = SDL_CreateTextureFromImage("Raycasting/images/Walls.bmp", renderer);
+    SDL_Rect dst = {x, y, 0, 0};
+    SDL_QueryTexture(texture, NULL, NULL, &dst.w, &dst.h);
+    SDL_RenderCopy(renderer, texture, NULL, &dst);
+}
+
 int main(int argc, char **argv){
     int status = EXIT_FAILURE;
-    SDL_Color red = {255, 0, 0, 255};
+    SDL_Color white = {255, 255, 255, 255};
     if (SDL_Init(SDL_INIT_VIDEO) != 0){
         fprintf(stderr, "Error in SDL_Init : %s\n", SDL_GetError());
         goto Quit;
@@ -16,35 +51,9 @@ int main(int argc, char **argv){
         goto Quit;
     }
     SDL_SetWindowTitle(window, "Game in SDL2");
-    SDL_Surface *tmp = SDL_LoadBMP("Raycasting/images/Background.bmp");
-    if (tmp == NULL){
-        fprintf(stderr, "Error in SDL_LoadBMP : %s\n", SDL_GetError());
-        goto Quit;
-    }
-    SDL_Texture *tmp2 = SDL_CreateTextureFromSurface(renderer, tmp);
-    SDL_FreeSurface(tmp);
-    if (tmp2 == NULL){
-        fprintf(stderr, "Error in SDL_CreateTextureFromSurface : %s\n", SDL_GetError());
-        goto Quit;
-    }
-    SDL_Rect dst = {0, 0, 0, 0};
-    SDL_QueryTexture(tmp2, NULL, NULL, &dst.w, &dst.h);
-    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, dst.w, dst.h);
-    if (texture == NULL){
-        fprintf(stderr, "Error in SDL_CreateTexture : %s\n", SDL_GetError());
-        goto Quit;
-    }
-    SDL_SetRenderTarget(renderer, texture);
-    SDL_RenderCopy(renderer, tmp2, NULL, NULL);
-    SDL_DestroyTexture(tmp2);
-    SDL_SetRenderTarget(renderer, NULL);
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
-    SDL_Point p1 = {225, 225}, p2 = {275, 275};
-    SDL_SetRenderTarget(renderer, texture);
-    SDL_SetRenderDrawColor(renderer, red.r, red.g, red.b, red.a);
-    SDL_RenderDrawLine(renderer, p1.x, p1.y, p2.x, p2.y);
-    SDL_SetRenderTarget(renderer, NULL);
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_SetRenderDrawColor(renderer, white.r, white.g, white.b, white.a);
+    SDL_RenderClear(renderer);
+    SDL_RenderDrawWall(renderer, 0, 0);
     SDL_RenderPresent(renderer);
     SDL_Event event;
     SDL_bool quit = SDL_FALSE;
@@ -54,8 +63,6 @@ int main(int argc, char **argv){
     }
     status = EXIT_SUCCESS;
     Quit:
-        if (tmp2 != NULL) SDL_DestroyTexture(tmp2);
-        if (texture != NULL) SDL_DestroyTexture(texture);
         if (renderer != NULL) SDL_DestroyRenderer(renderer);
         if (window != NULL) SDL_DestroyWindow(window);
         SDL_Quit();
